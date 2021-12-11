829a63ee170ac3c7e15d710320297496d82937df
Matthew J. Dovey <m.dovey@ceridwen.com>
2021-12-11 19:59:17 +0000

    Cleaned zxnext

--- zxnext/zxnext.srcs/sources_1/ip/sdpram_64_9/sdpram_64_9_stub.v
+++ zxnext/zxnext.srcs/sources_1/ip/sdpram_64_9/sdpram_64_9_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2021.2 (win64) Build 3367213 Tue Oct 19 02:48:09 MDT 2021
-// Date        : Mon Nov 15 17:10:08 2021
+// Date        : Sat Dec 11 19:45:24 2021
 // Host        : JL69XDHR2 running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
 //               v:/zxnext/zxnext/zxnext.srcs/sources_1/ip/sdpram_64_9/sdpram_64_9_stub.v
