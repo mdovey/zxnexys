a8a4489378fa4d13d7cfa0cf6c6a1624877fe06e
Matthew J. Dovey <m.dovey@ceridwen.com>
2021-12-11 12:09:27 +0000

    Fix licenses

--- pmod_rtcc/pmod_rtcc.srcs/sources_1/new/pmod_rtcc.v
+++ pmod_rtcc/pmod_rtcc.srcs/sources_1/new/pmod_rtcc.v
@@ -1,17 +1,20 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 30.09.2021 22:38:20
-// Design Name: 
-// Module Name: pmod_rrtc
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
+//
+// Copyright (C) 2021  Matthew J. Dovey
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // 
 // Revision:
 // Revision 0.01 - File Created
